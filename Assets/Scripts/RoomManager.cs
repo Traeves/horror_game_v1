b2162e55f354e16cd5e6ea9450fa9314a55c5101
GrimIsAlive <SAMUEL.WHITT03@GMAIL.COM>
2026-04-01 01:53:06 -0400

    freddy five bear and his loud ass

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public int Player_Room = -1;
+    public int Amelia_Room = -1;
+
+    //1 = Foyer
+    //2 = Right Exhibit
+    //3 = Left Exhibit
+    //4 = Exhibit 3
+    //5 = Exhibit 4
+    //6 = Back Room
+    //7 = Main Hallway
+    //8 = Right Hallway
+    //9 = Left Hallway
+    //10 = Right HW to E3
+    //11 = Left HW to E4
+    //12 = Right HW to BR
+    //13 = Left Hw to BR
+}
