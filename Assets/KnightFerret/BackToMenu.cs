6e6b5a6b605acd34597bb4acf8c71fbb16d66562
NateAsferi <nathanasferi0@gmail.com>
2026-04-28 23:40:59 -0400

    bleh

--- Assets/KnightFerret/BackToMenu.cs
+++ Assets/KnightFerret/BackToMenu.cs
@@ -1,10 +1,34 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class BackToMenu : MonoBehaviour
 {
-    public void StartGame()
+    public CanvasGroup fadeCanvas; // Drag your fade CanvasGroup here
+    public float fadeDuration = 1f;
+
+    public void TitleScreen()
     {
-        SceneManager.LoadScene("MainMenu");
+        StartCoroutine(FadeTo("MainMenu"));
+    }
+    public void Respawn()
+    {
+        StartCoroutine(FadeTo("BasicScene"));
+    }
+
+    IEnumerator FadeTo(string sceneName)
+    {
+        float elapsed = 0f;
+        fadeCanvas.alpha = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+            fadeCanvas.alpha = Mathf.Clamp01(elapsed / fadeDuration);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -1,28 +1,45 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using System.Collections;
 
 public class MenuManager : MonoBehaviour
 {
+    public CanvasGroup fadeCanvas;
+    public float fadeDuration = 1f;
+
     public void StartGame()
     {
         Debug.Log("START BUTTON ACTIVATED!");
-        SceneManager.LoadScene("BasicScene");
+        StartCoroutine(FadeTo("BasicScene"));
     }
-
     public void OpenCredits()
     {
         Debug.Log("Credits Clicked!");
-        SceneManager.LoadScene("Credits");
+        StartCoroutine(FadeTo("Credits"));
     }
-
     public void QuitGame()
     {
         Debug.Log("Quit Clicked!");
-
-        #if UNITY_EDITOR
+#if UNITY_EDITOR
             UnityEditor.EditorApplication.isPlaying = false;
-        #else
-                Application.Quit();
-        #endif
+#else
+        Application.Quit();
+#endif
+    }
+
+    IEnumerator FadeTo(string sceneName)
+    {
+        float elapsed = 0f;
+        fadeCanvas.alpha = 0f;
+
+        while (elapsed < fadeDuration)
+        {
+            elapsed += Time.unscaledDeltaTime;
+            fadeCanvas.alpha = Mathf.Clamp01(elapsed / fadeDuration);
+            yield return null;
+        }
+
+        SceneManager.LoadScene(sceneName);
     }
 }
